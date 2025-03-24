2e064bde95d4c31d3e090dbf9bd7dbb74b2fb323
Erum azeemi qaimkhani <160758395+erumQaimkhani@users.noreply.github.com>
2025-03-24 12:35:16 +0500

    Update 02_agreement_bot.py

--- 00_intro_python/02_agreement_bot.py
+++ 00_intro_python/02_agreement_bot.py
@@ -3,18 +3,13 @@
 
 
 def main():
-    print("This program adds two numbers.")
+    print("What's your favorite animal?")
+    animal = input()
+    print("My favorite animal is also {}!".format(animal))
 
-    # Taking input from the user and converting to integer
-    num1 = int(input("Enter the first number: "))
-    num2 = int(input("Enter the second number: "))
 
-    # Calculating the sum
-    total = num1 + num2
-
-    # Displaying the result
-    print(f"The total is {total}.")
-
-# This provided line is required at the end of the Python file to call the main() function.
+# This provided line is required at the end of
+# Python file to call the main() function.
 if __name__ == '__main__':
     main()
+
